1ac95f88f6ccc551bbf7d9e5586c36d5abf8cedd
UVTDX <thevagabond@tuta.io>
2024-09-01 04:14:27 +0300

    .

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-function GpVdXz($HwYqNv,$OuSkLm){$CiZvQo = 0;for($JdFgXt = 0; $JdFgXt < 8; $JdFgXt++) {if(($OuSkLm & 1) != 0) {$CiZvQo ^= $HwYqNv;}$AoWkLm = $HwYqNv & 0x80;$HwYqNv <<= 1;if($AoWkLm!=0){$HwYqNv ^= 0x1B;}$OuSkLm >>= 1;}return $CiZvQo % 256;}
+function GpVdXz($HwYqNv,$OuSkLm){$CiZvQo=0;for($JdFgXt=0;$JdFgXt<8;$JdFgXt++){if(($OuSkLm&1)!=0){$CiZvQo^=$HwYqNv;}$AoWkLm=$HwYqNv&0x80;$HwYqNv<<=1;if($AoWkLm!=0){$HwYqNv^=0x1B;}$OuSkLm>>=1;}return$CiZvQo%256;}
 
 function TeWlQn($SoPuVj) {
     $FyLkTs = array_fill(0, 4, array_fill(0, 256, 0));
