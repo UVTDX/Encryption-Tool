7f6ba848da7f454314f3aee0743023e3d238d37b
UVTDX <thevagabond@tuta.io>
2024-09-01 03:54:08 +0300

    .

--- main.php
+++ main.php
@@ -1,6 +1,31 @@
 <?php
- 
-$userInput = readline(' Enter your text: '); 
- 
-echo $userInput;     
+function GpVdXz($HwYqNv, $OuSkLm) {
+    $CiZvQo = 0;
+    for ($JdFgXt = 0; $JdFgXt < 8; $JdFgXt++) {
+        if (($OuSkLm & 1) != 0) {
+            $CiZvQo ^= $HwYqNv;
+        }
+        $AoWkLm = $HwYqNv & 0x80;
+        $HwYqNv <<= 1;
+        if ($AoWkLm != 0) {
+            $HwYqNv ^= 0x1B;
+        }
+        $OuSkLm >>= 1;
+    }
+    return $CiZvQo % 256;
+
+function TeWlQn($SoPuVj) {
+    $FyLkTs = array_fill(0, 4, array_fill(0, 256, 0));
+    $JrAoMv = strlen($SoPuVj);
+    $WbTfLm = [];
+    for ($YcEkQp = 0; $YcEkQp < $JrAoMv; $YcEkQp++) {
+        $WbTfLm[$YcEkQp] = ord($SoPuVj[$YcEkQp]);
+    }
+    for ($DrXyNb = 0; $DrXyNb < 4; $DrXyNb++) {
+        for ($LkOfSm = 0; $LkOfSm < 256; $LkOfSm++) {
+            $FyLkTs[$DrXyNb][$LkOfSm] = GpVdXz($WbTfLm[$DrXyNb], $LkOfSm);
+        }
+    }
+    return $FyLkTs;
+}
 ?> 
