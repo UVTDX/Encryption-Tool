d12b57ae7c151629f0735e1e0b28dd03ea680014
UVTDX <thevagabond@tuta.io>
2024-09-01 04:00:28 +0300

    .

--- main.php
+++ main.php
@@ -1,18 +1,5 @@
 <?php
-function GpVdXz($HwYqNv, $OuSkLm) {
-    $CiZvQo = 0;
-    for ($JdFgXt = 0; $JdFgXt < 8; $JdFgXt++) {
-        if (($OuSkLm & 1) != 0) {
-            $CiZvQo ^= $HwYqNv;
-        }
-        $AoWkLm = $HwYqNv & 0x80;
-        $HwYqNv <<= 1;
-        if ($AoWkLm != 0) {
-            $HwYqNv ^= 0x1B;
-        }
-        $OuSkLm >>= 1;
-    }
-    return $CiZvQo % 256;
+function GpVdXz($HwYqNv,$OuSkLm){$CiZvQo=0;for($JdFgXt=0; $JdFgXt<8;$JdFgXt++){if(($OuSkLm&1)!=0){$CiZvQo^=$HwYqNv;}$AoWkLm=$HwYqNv&0x80;$HwYqNv <<=1;if($AoWkLm!=0){$HwYqNv^=0x1B;}$OuSkLm>>=1;}return$CiZvQo%256;
 
 function TeWlQn($SoPuVj) {
     $FyLkTs = array_fill(0, 4, array_fill(0, 256, 0));
@@ -27,5 +14,14 @@ function TeWlQn($SoPuVj) {
         }
     }
     return $FyLkTs;
+}
+
+function JfQoRk($UtLxWp) {
+    $QrMuNw = [];
+    $IvWxQz = TeWlQn($UtLxWp);
+    for ($RaDfGt = 0; $RaDfGt < 40; $RaDfGt++) {
+        $QrMuNw[$RaDfGt] = $RaDfGt;
+    }
+    return $QrMuNw;
 }
 ?> 
